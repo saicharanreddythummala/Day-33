f7dcdffda498859a5132dd5ecab3e91d212a5c3d
Sai Charan Reddy <saicharanreddy1999.ct@gmail.com>
2022-07-15 22:42:50 +0530

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { createFileRouter } from "./routes/createFile.js";
 import { getFilesRouter } from "./routes/getFiles.js";
+import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -9,6 +10,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Home page");
